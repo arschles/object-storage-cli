561f98cdf3c7a0298737bd0946b49ab8504743ec
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-28 14:00:58 -0700

    completing the s3 driver constructor function

--- config/s3.go
+++ config/s3.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-  "io/ioutil"
+	"io/ioutil"
 
 	"github.com/deis/distribution/registry/storage/driver"
 	s3 "github.com/deis/distribution/registry/storage/driver/s3-aws"
@@ -11,30 +11,40 @@ type S3 struct {
 	AccessKeyFile string `envconfig:"ACCESS_KEY_FILE" default:"/var/run/secrets/deis/objectstore/creds/accesskey"`
 	SecretKeyFile string `envconfig:"SECRET_KEY_FILE" default:"/var/run/secrets/deis/objectstore/creds/secretkey"`
 	RegionFile    string `envconfig:"REGION_FILE" default:"/var/run/secrets/deis/objectstore/creds/region"`
-  BucketFile string `envconfig:"BUCKET_FILE" default:"/var/run/secrets/deis/objectstore/creds/builder-bucket"`
+	BucketFile    string `envconfig:"BUCKET_FILE" default:"/var/run/secrets/deis/objectstore/creds/builder-bucket"`
+}
+
+func endpointURL(region string) string {
+	return fmt.Sprintf("s3-%s.amazonaws.com", region)
 }
 
 func (s S3) CreateDriver() (driver.StorageDriver, error) {
-  keyBytes, err := ioutil.ReadFile(s.AccessKeyFile)
-  if err != nil {
-    return nil, err
-  }
-  secretBytes, err := ioutil.ReadFile(s.SecretKeyFile)
-  if err != nil {
-    return nil, err
-  }
-  regionBytes, err := ioutil.ReadFile(s.RegionFile)
-  if err != nil {
-    return nil, err
-  }
-  bucketBytes, err := ioutil.ReadFile(s.BucketFile)
-  if err != nil {
-    return nil, err
-  }
-  params := s3.DriverParameters{
-    AccessKey: string(keyBytes),
-    SecretKey: string(secretBytes),
-    Bucket: string(bucketBytes),
-    Region: string(regionBytes)
-  }
+	keyBytes, err := ioutil.ReadFile(s.AccessKeyFile)
+	if err != nil {
+		return nil, err
+	}
+	secretBytes, err := ioutil.ReadFile(s.SecretKeyFile)
+	if err != nil {
+		return nil, err
+	}
+	regionBytes, err := ioutil.ReadFile(s.RegionFile)
+	if err != nil {
+		return nil, err
+	}
+	bucketBytes, err := ioutil.ReadFile(s.BucketFile)
+	if err != nil {
+		return nil, err
+	}
+	params := s3.DriverParameters{
+		AccessKey:   string(keyBytes),
+		SecretKey:   string(secretBytes),
+		Bucket:      string(bucketBytes),
+		Region:      string(regionBytes),
+		EndpointURL: endpointURL(string(regionBytes)),
+	}
+	d, err := s3.New(params)
+	if err != nil {
+		return nil, err
+	}
+	return d, nil
 }
