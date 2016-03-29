1a81129288454a4fa63d957b8142daf28cca4515
Aaron Schlesinger <aschlesinger@deis.com>
2016-03-29 09:54:01 -0700

    adding convenience functions for config
    to convert strings and StorageTypes to a config

--- config/config.go
+++ config/config.go
@@ -1,8 +1,14 @@
 package config
 
 import (
+	"fmt"
+
 	driver "github.com/docker/distribution/registry/storage/driver"
 	"github.com/kelseyhightower/envconfig"
+)
+
+const (
+	appName = "objstorage"
 )
 
 // Config is the generic interface from which a storage driver can be created
@@ -10,9 +16,41 @@ type Config interface {
 	CreateDriver() (driver.StorageDriver, error)
 }
 
-// GetConfigFromStorageType gets a Config implementation from the st param. If there's no known config
-// for the given type, returns an appropriate error
-func GetConfigFromStorageType(st StorageType) (Config, error) {
-	// NYI
-	return nil, nil
+type ErrUnsupportedStorageType struct {
+	st StorageType
+}
+
+func (e ErrUnsupportedStorageType) Error() string {
+	return fmt.Sprintf("Unsupported storage type %s", e.st)
+}
+
+// FromStorageType gets a Config implementation from the st param. If there's no known config for the given type, return ErrUnsupportedStorageType. Returns an appropriate error in all other error cases.
+func FromStorageType(st StorageType) (Config, error) {
+	var conf Config
+	switch st {
+	case S3StorageType:
+		conf = new(S3)
+	case GCSStorageType:
+		conf = new(GCS)
+	case AzureStorageType:
+		conf = new(Azure)
+	case EmptyStorageType:
+		conf = new(Empty)
+	default:
+		return nil, ErrUnsupportedStorageType{st: st}
+	}
+
+	if err := envconfig.Process(appName, conf); err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+// FromStorageTypeString calls StorageTypeFromString, then calls FromStorageType to get the Config from that storage type. Returns an appropriate error on all failures
+func FromStorageTypeString(str string) (Config, error) {
+	st, err := StorageTypeFromString(str)
+	if err != nil {
+		return nil, err
+	}
+	return FromStorageType(st)
 }
